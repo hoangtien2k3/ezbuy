aabd48b2f5369a974f010529cb79ab26befd0136
hoangtien2k3 <hoangtien2k3qx1@gmail.com>
2025-03-30 19:13:05 +0700

    ezbuy-server

---
+++ reactify-core/src/main/java/com/reactify/ReactifyAutoConfiguration.java
@@ -0,0 +1,23 @@
+package com.reactify;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Auto-configuration for the Reactify library.
+ * <p>
+ * Automatically scans and registers components in the <code>com.reactify</code> package,
+ * enabling seamless integration with Spring Boot.
+ * </p>
+ *
+ * <h3>Usage:</h3>
+ * <pre>
+ * Add the Reactify dependency, and Spring Boot will configure its components automatically.
+ * </pre>
+ *
+ * @author @hoangtien2k3
+ * @since 1.0
+ */
+@Configuration
+@ComponentScan(basePackages = "com.reactify")
+public class ReactifyAutoConfiguration {}
