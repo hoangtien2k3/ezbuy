ad3411a20aaed325d9a98c519126b516909695e7
hoangtien2k3 <hoangtien2k3qx1@gmail.com>
2025-05-18 15:32:28 +0700

    ezbuy-server

---
+++ eureka-service/src/main/java/com/ezbuy/eurekaservice/EurekaServiceApplication.java
@@ -0,0 +1,27 @@
+package com.ezbuy.eurekaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication(
+        exclude = {
+                DataSourceAutoConfiguration.class,
+                DataSourceTransactionManagerAutoConfiguration.class,
+                HibernateJpaAutoConfiguration.class,
+                SecurityAutoConfiguration.class,
+                R2dbcAutoConfiguration.class
+        })
+@EnableScheduling
+@EnableEurekaServer
+public class EurekaServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServiceApplication.class, args);
+    }
+}
