ff2f3cea774f12d76f2a0efd4a14516a4b11856a
hoangtien2k3 <hoangtien2k3qx1@gmail.com>
2025-11-22 23:59:22 +0700

    Refactor deployment scripts and CI configurations for ezbuy services; update service names and streamline Docker image builds for improved consistency and maintainability

--- scripts/buildAllImage.sh
+++ scripts/buildAllImage.sh
@@ -1,45 +1,26 @@
 #chmod +x buildAllImage.sh
-BASE_IMAGE="localhost:8956"
-# shellcheck disable=SC2034
-API_GATEWAY_SERVICE=gateway-service-sandbox:1.0.2
-AUTH_SERVICE=auth-service-sandbox:1.0.3
-# shellcheck disable=SC2034
-CART_VERSION=cart-service-sandbox:1.0.2
-# shellcheck disable=SC2034
-CUSTOMER_VERSION=customer-service-sandbox:1.0.3
-# shellcheck disable=SC2034
-NOTIFICATION_VERSION=noti-service-sandbox:1.0.3
-# shellcheck disable=SC2034
-ORDER_VERSION=order-service-sandbox:1.0.3
-# shellcheck disable=SC2034
-PAYMENT_VERSION=payment-service-sandbox:1.0.3
-# shellcheck disable=SC2034
-PRODUCT_VERSION=product-service-sandbox:1.0.3
-# shellcheck disable=SC2034
-RATING_VERSION=rating-service-sandbox:1.0.3
-# shellcheck disable=SC2034
-SEARCH_VERSION=search-service-sandbox:1.0.3
-# shellcheck disable=SC2034
-SEND_NOTIFICATION_VERSION=send-noti-service-sandbox:1.0.4
-# shellcheck disable=SC2034
-SETTING_VERSION=setting-service-sandbox:1.0.3
-# shellcheck disable=SC2034
-SYNC_VERSION=sync-service-sandbox:1.0.3
-# shellcheck disable=SC2034
-TRIGGER_VERSION=trigger-service-sandbox:1.0.3
+BASE_IMAGE="hoangtien2k3"
+EZBUY_AUTH=ezbuy-auth:lastest
+EZBUY_CART=ezbuy-cart:lastest
+EZBUY_GATEWAY=ezbuy-gateway:lastest
+EZBUY_NOTI=ezbuy-noti:lastest
+EZBUY_ORDER=ezbuy-order:lastest
+EZBUY_PAYMENT=ezbuy-payment:lastest
+EZBUY_PRODUCT=ezbuy-product:lastest
+EZBUY_RATING=ezbuy-rating:lastest
+EZBUY_SEARCH=ezbuy-search:lastest
+EZBUY_SETTING=ezbuy-setting:lastest
+EZBUY_TRIGGER=ezbuy-trigger:lastest
 
 #cd ../ && mvn clean install
-cd ../api-gateway-service && mvn clean package && docker build -t $BASE_IMAGE/$API_GATEWAY_SERVICE . && docker push $BASE_IMAGE/$API_GATEWAY_SERVICE
-cd ../auth-service && mvn clean package && docker build -t $BASE_IMAGE/$AUTH_SERVICE . && docker push $BASE_IMAGE/$AUTH_SERVICE
-cd ../cart-service && mvn clean package && docker build -t $BASE_IMAGE/$CART_VERSION . && docker push $BASE_IMAGE/$CART_VERSION
-cd ../customer-service && mvn clean package && docker build -t $BASE_IMAGE/$CUSTOMER_VERSION . && docker push $BASE_IMAGE/$CUSTOMER_VERSION
-cd ../notification-service && mvn clean package && docker build -t $BASE_IMAGE/$NOTIFICATION_VERSION . && docker push $BASE_IMAGE/$NOTIFICATION_VERSION
-cd ../order-service && mvn clean package && docker build -t $BASE_IMAGE/$ORDER_VERSION . && docker push $BASE_IMAGE/$ORDER_VERSION
-cd ../payment-service && mvn clean package && docker build -t $BASE_IMAGE/$PAYMENT_VERSION . && docker push $BASE_IMAGE/$PAYMENT_VERSION
-cd ../product-service && mvn clean package && docker build -t $BASE_IMAGE/$PRODUCT_VERSION . && docker push $BASE_IMAGE/$PRODUCT_VERSION
-cd ../rating-service && mvn clean package && docker build -t $BASE_IMAGE/$RATING_VERSION . && docker push $BASE_IMAGE/$RATING_VERSION
-cd ../search-service && mvn clean package && docker build -t $BASE_IMAGE/$SEARCH_VERSION . && docker push $BASE_IMAGE/$SEARCH_VERSION
-cd ../send-notification-service && mvn clean package && docker build -t $BASE_IMAGE/$SEND_NOTIFICATION_VERSION . && docker push $BASE_IMAGE/$SEND_NOTIFICATION_VERSION
-cd ../setting-service && mvn clean package && docker build -t $BASE_IMAGE/$SETTING_VERSION . && docker push $BASE_IMAGE/$SETTING_VERSION
-cd ../sync-service && mvn clean package && docker build -t $BASE_IMAGE/$SYNC_VERSION . && docker push $BASE_IMAGE/$SYNC_VERSION
-cd ../trigger-service && mvn clean package && docker build -t $BASE_IMAGE/$TRIGGER_VERSION . && docker push $BASE_IMAGE/$TRIGGER_VERSION
+cd ../ezbuy-auth && mvn clean package && docker build -t $BASE_IMAGE/$EZBUY_AUTH . && docker push $BASE_IMAGE/$EZBUY_AUTH
+cd ../ezbuy-cart && mvn clean package && docker build -t $BASE_IMAGE/$EZBUY_CART . && docker push $BASE_IMAGE/$EZBUY_CART
+cd ../ezbuy-gateway && mvn clean package && docker build -t $BASE_IMAGE/$EZBUY_GATEWAY . && docker push $BASE_IMAGE/$EZBUY_GATEWAY
+cd ../ezbuy-noti && mvn clean package && docker build -t $BASE_IMAGE/$EZBUY_NOTI . && docker push $BASE_IMAGE/$EZBUY_NOTI
+cd ../ezbuy-order && mvn clean package && docker build -t $BASE_IMAGE/$EZBUY_ORDER . && docker push $BASE_IMAGE/$EZBUY_ORDER
+cd ../ezbuy-payment && mvn clean package && docker build -t $BASE_IMAGE/$EZBUY_PAYMENT . && docker push $BASE_IMAGE/$EZBUY_PAYMENT
+cd ../ezbuy-product && mvn clean package && docker build -t $BASE_IMAGE/$EZBUY_PRODUCT . && docker push $BASE_IMAGE/$EZBUY_PRODUCT
+cd ../ezbuy-rating && mvn clean package && docker build -t $BASE_IMAGE/$EZBUY_RATING . && docker push $BASE_IMAGE/$EZBUY_RATING
+cd ../ezbuy-search && mvn clean package && docker build -t $BASE_IMAGE/$EZBUY_SEARCH . && docker push $BASE_IMAGE/$EZBUY_SEARCH
+cd ../ezbuy-setting && mvn clean package && docker build -t $BASE_IMAGE/$EZBUY_SETTING . && docker push $BASE_IMAGE/$EZBUY_SETTING
+cd ../ezbuy-trigger && mvn clean package && docker build -t $BASE_IMAGE/$EZBUY_TRIGGER . && docker push $BASE_IMAGE/$EZBUY_TRIGGER
--- workflows.sh
+++ workflows.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
 WORKFLOW_TEMPLATE=$(cat .github/workflow-template.yaml)
-SERVICES=(auth-service cart-service notification-send notification-service order-service payment-service product-service rating-service search-service setting-service trigger-service)
+SERVICES=(
+  ezbuy-auth
+  ezbuy-cart
+  ezbuy-gateway
+  ezbuy-noti
+  ezbuy-order
+  ezbuy-payment
+  ezbuy-product
+  ezbuy-rating
+  ezbuy-search
+  ezbuy-setting
+  ezbuy-trigger
+)
 
 for SERVICE_NAME in "${SERVICES[@]}"; do
     echo "Generating workflow for ${SERVICE_NAME} service"
-
     WORKFLOW="${WORKFLOW_TEMPLATE//\{\{SERVICE_NAME\}\}/${SERVICE_NAME}}"
     echo "${WORKFLOW}" > ".github/workflows/${SERVICE_NAME}.yaml"
 done
